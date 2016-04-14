074b9ee7220fe730100e3ef5201f697871ff4ae2
raj <yaramguf14109@gmail.com>
2016-04-14 17:16:58 -0400

    jhgfdszZxcvbn

--- src/com/nexify/controller/HelloController.java
+++ src/com/nexify/controller/HelloController.java
@@ -19,6 +19,7 @@ public class HelloController {
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from HelloWorld.java **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
+messagekjfchvb 
 	}
 }
 
